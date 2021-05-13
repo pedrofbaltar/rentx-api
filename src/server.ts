71851843023770d605dd063b45715b0b4e7864fd
pedrofbaltar <pfbaltariead@gmai.com>
2021-05-13 15:40:06 -0300

    Creating category

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello world" });
-});
+app.use(express.json());
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running! 🚀"));
