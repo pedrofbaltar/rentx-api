ed66ae33af4d061020e770621a0822f060fd007c
pedrofbaltar <pfbaltariead@gmail.com>
2021-05-17 15:46:09 -0300

    Creating services specifications and separating modules

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../repositories/CategoriesRepositories";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepositories";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
