39d9e4537b92a04195a2247bcdb47a5b6dacac47
pedrofbaltar <pfbaltariead@gmail.com>
2021-05-31 16:11:30 -0300

    Creating categories migration

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,9 @@ interface IOptions {
 
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
+
   newOptions.host = "database";
+
   createConnection({
     ...options,
   });
