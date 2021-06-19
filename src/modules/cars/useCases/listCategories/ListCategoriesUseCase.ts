71201110e8e4d540479fa957008587115a1d48e4
pedrofbaltar <pfbaltariead@gmail.com>
2021-06-19 18:01:46 -0300

    Refactoring specifications

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,10 +1,14 @@
+import { inject } from "tsyringe";
 import { Category } from "../../entities/Category";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 class ListCategoriesUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
-  execute(): Category[] {
+  async execute(): Promise<Category[]> {
     const categories = this.categoriesRepository.list();
 
     return categories;
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,13 +1,9 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
-import { CreateSpecificationUseCase } from "../modules/cars/useCases/createSpecification/CreateSpecificationUseCase";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationsRepository();
+const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.post("/", (req, res) => {
-  return createSpecificationController.handle(req, res);
-});
+specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
